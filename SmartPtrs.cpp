92ec5c8a0a49c4b6eab70e49716c2ea362305283
vecstor <vaibhav.rumale@gmail.com>
2024-04-15 23:10:04 -0700

    SmartPtrs

---
+++ SmartPtrs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <memory>
+#include <string>
+class entity {
+
+public:
+  entity() { std::cout << "created entity" << std::endl; }
+  ~entity() { std::cout << "destroyed entity" << std::endl; }
+  void print() { std::cout << "print" << std::endl; }
+};
+int main() {
+  std::shared_ptr<entity> e0;
+  {
+    std::shared_ptr<entity> sharedEntity = std::make_shared<entity>();
+    e0 = sharedEntity;
+    sharedEntity->print();
+  }
+}
