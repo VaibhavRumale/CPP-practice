e53398964029d113fc5cde7345a80c3ca97050bd
vecstor <vaibhav.rumale@gmail.com>
2024-05-17 15:12:55 -0700

    Simple template

---
+++ templateA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+template <typename T> T my_Max(T x, T y)
+{
+return (x>y) ? x : yl;
+    }
+
+
+
+int main(){
+
+
+
+	cout<< my_Max<int>(100, 200) << endl;
+	return 0;
+
+    }
